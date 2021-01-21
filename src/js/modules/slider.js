4e8087d437b5f2e83c66a7cee2a0aff49aef1bb1
BorisBoko <borispetrosyans8@gmail.com>
2021-01-21 14:35:04 +0400

    slide with out Global Slide

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,10 +15,25 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000)
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (error) {}
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
             this.slides[this.slideIndex - 1].style.display = 'block';
         });
+
+
     }
 
     plusSlides(n) {
@@ -26,13 +41,25 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (e) {}
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
             });
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
         });
 
         this.showSlides(this.slideIndex);
+
+
     }
 
 }
